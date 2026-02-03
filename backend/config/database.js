7e0208cbb4dc6769c0cff76233357023d48dee49
dobryakk5 <system5@mail.ru>
2026-02-03 16:55:20 +0200

    fix

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,11 +3,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    user: process.env.DB_USER || 'postgres',
-    host: process.env.DB_HOST || 'localhost',
-    database: process.env.DB_NAME || 'russian_tests_db',
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT || 5432,
+    connectionString: process.env.DATABASE_URL,
 });
 
 // Проверка подключения
